bdd89ee6adeabe939877efc2289c6158c8a557fb
llallonen <llallonen@gmail.com>
2023-10-26 11:57:54 +0300

    feat: start add product page

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,64 @@
+import prisma from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Add Product - cozystore",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageURL")?.toString();
+  const price = Number(formData.get("price"));
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields values");
+  }
+
+  await prisma.products.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect('/')
+}
+
+export default function AddProductPage() {
+  return (
+    <div>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form>
+        <input
+          required
+          name="name"
+          placeholder="Name"
+          className="input input-bordered mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="imageURL"
+          placeholder="image URL"
+          type="url"
+          className="input input-bordered mb-3 w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+          className="input input-bordered mb-3 w-full"
+        />
+        <button className="btn btn-primary btn-block" type="submit">
+          Add product
+        </button>
+      </form>
+    </div>
+  );
+}
