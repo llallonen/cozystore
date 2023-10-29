980cf31c5a6a986c33f2507a38034d9f96a32718
llallonen <llallonen@gmail.com>
2023-10-29 16:38:21 +0300

    feat: update product card component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -15,7 +15,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
 
   return (
     <Link
-      href={"/products" + product.id}
+      href={"/products/" + product.id}
       className="card w-full bg-base-200 transition-shadow hover:shadow-md"
     >
       <figure>
