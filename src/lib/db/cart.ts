143a1735f58cd6e26f54ca188eeaf3a625d8a1a4
llallonen <llallonen@gmail.com>
2023-10-29 15:01:15 +0300

    feat: start working with cart entity

---
+++ src/lib/db/cart.ts
@@ -0,0 +1,39 @@
+import { Cart } from "@prisma/client";
+import prisma from "./prisma";
+import { cookies } from "next/dist/client/components/headers";
+
+export type ShoppingCart = Cart & {
+    size: number,
+    subtotal: number
+}
+
+export async function getCart() {
+  const localCartId = cookies().get("localCartId")?.value;
+  const cart = localCartId
+    ? await prisma.cart.findUnique({
+        where: { id: localCartId },
+        include: { items: { include: { product: true } } },
+      })
+    : null;
+
+  if (!cart) {
+    return null;
+  }
+
+  return {
+    ...cart,
+    size: cart.items.reduce((acc, item) => acc + item.quantity, 0),
+    subtotal: cart.items.reduce(
+      (acc, item) => acc + item.quantity * item.product.price,
+      0,
+    ),
+  };
+}
+
+export async function createCart() {
+  const newCart = await prisma.cart.create({
+    data: {},
+  });
+
+  cookies().set("localCartId", newCart.id);
+}
