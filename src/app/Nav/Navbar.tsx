0b83a0388b1e1131f0ef113f57daa7b37c275360
llallonen <llallonen@gmail.com>
2023-10-31 00:07:41 +0300

    feat: start work with navigation

---
+++ src/app/Nav/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import logo from "@/assets/logo.svg";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="bg-base-300">
+      <div className="navbar m-auto max-w-6xl flex-col gap-2 sm:flex-row">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl normal-case">
+            <Image src={logo} height={40} width={40} alt="cozystore" />
+          </Link>
+        </div>
+        <div className="flex-none gap-2">
+          <form>
+            <div className="form-control">
+              <input
+                name="search"
+                placeholder="Search"
+                className="input input-bordered w-full min-w-[100px]"
+              ></input>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
