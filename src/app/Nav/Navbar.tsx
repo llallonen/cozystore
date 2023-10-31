c511c58fd03cc7eaa771f2b18e494e7300b83345
llallonen <llallonen@gmail.com>
2023-10-31 18:25:45 +0300

    feat: upd nav bar and cart button

--- src/app/Nav/Navbar.tsx
+++ src/app/Nav/Navbar.tsx
@@ -1,8 +1,23 @@
 import Link from "next/link";
 import logo from "@/assets/logo.svg";
 import Image from "next/image";
+import { redirect } from "next/navigation";
+import { getCart } from "@/lib/db/cart";
+import CartBtn from "./CartBtn";
 
-const Navbar = () => {
+async function searchProduct(formData: FormData) {
+  "use server";
+
+  const searchQuery = formData.get("search")?.toString();
+
+  if (searchQuery) {
+    redirect("/search?query=" + searchQuery);
+  }
+}
+
+export default async function Navbar() {
+  const cart = await getCart();
+
   return (
     <div className="bg-base-300">
       <div className="navbar m-auto max-w-6xl flex-col gap-2 sm:flex-row">
@@ -12,7 +27,7 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="flex-none gap-2">
-          <form>
+          <form action={searchProduct}>
             <div className="form-control">
               <input
                 name="search"
@@ -21,10 +36,9 @@ const Navbar = () => {
               ></input>
             </div>
           </form>
+          <CartBtn cart={cart} />
         </div>
       </div>
     </div>
   );
-};
-
-export default Navbar;
+}
