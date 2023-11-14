1015b5df34c656f0e1febadade531fa799d40e44
llallonen <llallonen@gmail.com>
2023-11-14 19:20:00 +0300

    feat: update total calculation action

--- src/app/products/[id]/actions.ts
+++ src/app/products/[id]/actions.ts
@@ -4,25 +4,32 @@ import { createCart, getCart } from "@/lib/db/cart";
 import prisma from "@/lib/db/prisma";
 import { revalidatePath } from "next/cache";
 
-export async function incProductQuatity(productId: string) {
+export async function setProductQuatity(productId: string, quantity: number) {
   const cart = (await getCart()) ?? (await createCart());
 
   const productInCart = cart.items.find((item) => item.productId === productId);
 
-  if (productInCart) {
-    await prisma.cartItem.update({
-      where: { id: productInCart.id },
-      data: { quantity: { increment: 1 } },
-    });
+  if (quantity === 0) {
+    if (productInCart) {
+      await prisma.cartItem.delete({
+        where: { id: productInCart.id },
+      });
+    }
   } else {
-    await prisma.cartItem.create({
-      data: {
-        cartId: cart.id,
-        productId,
-        quantity: 1,
-      },
-    });
+    if (productInCart) {
+      await prisma.cartItem.update({
+        where: { id: productInCart.id },
+        data: { quantity },
+      });
+    } else {
+      await prisma.cartItem.create({
+        data: {
+          cartId: cart.id,
+          productId,
+          quantity: 1,
+        },
+      });
+    }
   }
-
   revalidatePath("/products/[id]");
 }
